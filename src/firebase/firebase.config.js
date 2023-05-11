9a56f25f5144d434a62724783123cfc57c5bd5a8
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-05-11 18:31:34 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBliv4-GHfXrBzb_VVkVy6A-eEi7jvciVQ",
+  authDomain: "car-doctor-project-2afb0.firebaseapp.com",
+  projectId: "car-doctor-project-2afb0",
+  storageBucket: "car-doctor-project-2afb0.appspot.com",
+  messagingSenderId: "974565179266",
+  appId: "1:974565179266:web:d757747b7175c9ed8a999b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
